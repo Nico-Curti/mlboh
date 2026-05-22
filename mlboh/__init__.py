7d86a96e473b255dfdb9138f5219cc0dbf5a4ef2
Nico Curti <nico.curti2@unibo.it>
2026-05-22 11:03:22 +0200

    [PY] add init of package and first versioning

---
+++ mlboh/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from .__version__ import __version__
+
+__author__ = ['Nico Curti']
+__email__ = ['nico.curti2@unibo.it']
+
+__all__ = [
+    '__version__',
+]
